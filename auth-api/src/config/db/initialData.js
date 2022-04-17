8d8ef9779351c4863c8b99c13341f81ec89dc257
Francisco Correia <franciscocorreia383@gmail.com>
2022-04-16 23:45:08 -0300

    Created User model and connection with database

---
+++ auth-api/src/config/db/initialData.js
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt'
+import User from '../../modules/user/model/User'
+
+export async function CreateInitialData() {
+
+    try {
+        await User.sync({ force: true });
+
+        let password = await bcrypt.hash('123456', 10);
+    
+        await User.create({
+            name: 'User Teste',
+            email: 'testeuser@gmail.com',
+            password: password
+        });
+    } catch (error) {
+        console.log(error.messege);
+    }
+}
